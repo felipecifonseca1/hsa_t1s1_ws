6b2e5e8971f0ee6f054bc9754d700f7ec4658c1a
Felipe <felipe.cifonseca@usp.br>
2026-04-23 14:01:03 +0000

    mirror_code

--- src/gpio_c/src/applications/main_mirror.c
+++ src/gpio_c/src/applications/main_mirror.c
@@ -3,11 +3,27 @@
 
 // For delay functions: F_CPU has to be defined
 #include <util/delay.h>
-
+# define F_CPU 1000000UL
 
 int main (void)
-{
+{   
 
+    DDRC &= ~(1 << PC4); // Set pin 4 of PORTC as input
+    DDRC |= (1 << PC3); // Set pin 3 of PORTC as output
+    while (1){
+        if(PINC & (1 << PC4))
+        {
+            // set output of pin 1 on port C to high
+            PORTC |= (1 << PC3);
+        }
+        else
+        {
+            // set output of pin 1 on port C to low
+            PORTC &= ~(1 << PC3);
+
+        }
+        _delay_ms(1000); // Wait for 1 second
+    }
     // Should never be reached
     return 0;
 }
